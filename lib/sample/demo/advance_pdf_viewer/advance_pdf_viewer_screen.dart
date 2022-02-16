1dbdcfab7e2f76d4ce15030ad80f9a6dcea52c1f
Loitp <freuss47@gmail.com>
2022-02-16 11:03:34 +0700

    update

--- lib/sample/demo/advance_pdf_viewer/advance_pdf_viewer_screen.dart
+++ lib/sample/demo/advance_pdf_viewer/advance_pdf_viewer_screen.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
-import 'package:hello_word/lib/util/uI_utils.dart';
+import 'package:flutter/material.dart';
 import 'package:hello_word/lib/util/url_launcher_utils.dart';
 
 class AdvancePDFViewerScreen extends StatefulWidget {
