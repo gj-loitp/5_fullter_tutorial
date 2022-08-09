06137edc56f42a7dee07b730cb655eb1e1e8f6a8
loitp <loitp@galaxy.one>
2022-08-09 13:45:48 +0700

    update game

--- lib/sample/game/ray_world/components/player.dart
+++ lib/sample/game/ray_world/components/player.dart
@@ -1,4 +1,5 @@
 import 'package:flame/components.dart';
+import 'package:flame/sprite.dart';
 import 'package:hello_word/lib/game/helpers/direction.dart';
 
 /**
@@ -8,10 +9,17 @@ import 'package:hello_word/lib/game/helpers/direction.dart';
  * +840766040293
  * freuss47@gmail.com
  */
-class Player extends SpriteComponent with HasGameRef {
+class Player extends SpriteAnimationComponent with HasGameRef {
   static final _size = 50.0;
   final double _playerSpeed = 300.0;
   Direction direction = Direction.none;
+
+  final double _animationSpeed = 0.15;
+  late final SpriteAnimation _runDownAnimation;
+  late final SpriteAnimation _runLeftAnimation;
+  late final SpriteAnimation _runUpAnimation;
+  late final SpriteAnimation _runRightAnimation;
+  late final SpriteAnimation _standingAnimation;
 
   Player()
       : super(
@@ -22,8 +30,27 @@ class Player extends SpriteComponent with HasGameRef {
   Future<void> onLoad() async {
     super.onLoad();
 
-    sprite = await gameRef.loadSprite('player.png');
+    // sprite = await gameRef.loadSprite('player.png');
     position = gameRef.size / 2 - Vector2.all(_size / 2);
+    _loadAnimations().then((_) => {animation = _standingAnimation});
+  }
+
+  Future<void> _loadAnimations() async {
+    final spriteSheet = SpriteSheet(
+      image: await gameRef.images.load('player_spritesheet.png'),
+      srcSize: Vector2(29.0, 32.0),
+    );
+
+    _runDownAnimation =
+        spriteSheet.createAnimation(row: 0, stepTime: _animationSpeed, to: 4);
+    _runUpAnimation =
+        spriteSheet.createAnimation(row: 2, stepTime: _animationSpeed, to: 4);
+    _runLeftAnimation =
+        spriteSheet.createAnimation(row: 1, stepTime: _animationSpeed, to: 4);
+    _runRightAnimation =
+        spriteSheet.createAnimation(row: 3, stepTime: _animationSpeed, to: 4);
+    _standingAnimation =
+        spriteSheet.createAnimation(row: 0, stepTime: _animationSpeed, to: 1);
   }
 
   // update is a function unique to Flame components.
@@ -41,18 +68,23 @@ class Player extends SpriteComponent with HasGameRef {
   void movePlayer(double delta) {
     switch (direction) {
       case Direction.up:
+        animation = _runUpAnimation;
         moveUp(delta);
         break;
       case Direction.down:
+        animation = _runDownAnimation;
         moveDown(delta);
         break;
       case Direction.left:
+        animation = _runLeftAnimation;
         moveLeft(delta);
         break;
       case Direction.right:
+        animation = _runRightAnimation;
         moveRight(delta);
         break;
       case Direction.none:
+        animation = _standingAnimation;
         break;
     }
   }
