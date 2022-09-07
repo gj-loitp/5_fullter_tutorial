5c218b5aed7eaf66618f7880c82cabc41653d09f
loitp <loitp@galaxy.one>
2022-09-07 10:29:52 +0700

    update

--- lib/sample/widget/button/flutter_speed_dial/flutter_speed_dial_screen.dart
+++ lib/sample/widget/button/flutter_speed_dial/flutter_speed_dial_screen.dart
@@ -1,7 +1,7 @@
+import 'package:com.roy93group.flutter_tutorial/lib/core/base_stateful_state.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
-import 'package:com.roy93group.flutter_tutorial/lib/core/base_stateful_state.dart';
 
 /**
  * Created by Loitp on 08,August,2022
@@ -367,7 +367,7 @@ class _FlutterSpeedDialScreenState
           // activeBackgroundColor: Colors.blue,
           elevation: 8.0,
           isOpenOnStart: false,
-          animationSpeed: 200,
+          animationDuration: Duration(milliseconds: 200),
           shape: customDialRoot
               ? const RoundedRectangleBorder()
               : const StadiumBorder(),
