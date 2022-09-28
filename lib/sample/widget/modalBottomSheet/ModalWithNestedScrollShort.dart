d4977091bdb131a22d8aa56bae1999e67a1b5221
loitp <loitp@galaxy.one>
2022-09-28 17:09:38 +0700

    update

---
+++ lib/sample/widget/modalBottomSheet/ModalWithNestedScrollShort.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+
+/**
+ * Created by Loitp on 08,August,2022
+ * Galaxy One company,
+ * Vietnam
+ * +840766040293
+ * freuss47@gmail.com
+ */
+class CustomModal extends StatelessWidget {
+  const CustomModal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var list = <String>[];
+    for (int i = 0; i < 7; i++) {
+      list.add("Loitp $i");
+    }
+
+    double heightRow = 50;
+    double heightTotal = heightRow * list.length;
+    double heightScreen = Get.height;
+    double heightFinal = 0;
+    if (heightTotal >= heightScreen) {
+      heightFinal = heightScreen;
+    } else {
+      heightFinal = heightTotal;
+    }
+
+    return Container(
+      height: heightFinal,
+      child: ListView.builder(
+        physics: BouncingScrollPhysics(),
+        controller: ModalScrollController.of(context),
+        itemBuilder: (context, index) {
+          return Container(
+            height: heightRow,
+            color: index.isOdd ? Colors.green : Colors.orange,
+          );
+        },
+        itemCount: list.length,
+      ),
+    );
+  }
+}
--- lib/sample/widget/modalBottomSheet/model_bottom_sheet_screen.dart
+++ lib/sample/widget/modalBottomSheet/model_bottom_sheet_screen.dart
@@ -1,19 +1,20 @@
+import 'package:com.roy93group.flutter_tutorial/lib/common/const/DimenConstants.dart';
+import 'package:com.roy93group.flutter_tutorial/lib/util/UIUtils.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:com.roy93group.flutter_tutorial/lib/common/const/DimenConstants.dart';
-import 'package:com.roy93group.flutter_tutorial/lib/util/UIUtils.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 import 'CircularModal.dart';
 import 'FloatingModal.dart';
 import 'ModalComplexAll.dart';
 import 'ModalFit.dart';
-import 'modal_inside_modal.dart';
 import 'ModalWillScope.dart';
 import 'ModalWithNavigator.dart';
 import 'ModalWithNestedScroll.dart';
+import 'ModalWithNestedScrollShort.dart';
 import 'ModalWithPageView.dart';
+import 'modal_inside_modal.dart';
 
 /**
  * Created by Loitp on 08,August,2022
@@ -175,6 +176,16 @@ class ModelBottomSheetScreen extends StatelessWidget {
                   expand: true,
                   context: context,
                   builder: (context) => ModalWithPageView(),
+                );
+              },
+            ),
+            UIUtils.getButton(
+              "Modal Custom",
+              () {
+                showCupertinoModalBottomSheet(
+                  expand: false,
+                  context: context,
+                  builder: (context) => CustomModal(),
                 );
               },
             ),
