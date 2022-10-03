f2a0b52f47ca0e8c4101a443566fa8cf86369723
loitp <loitp@galaxy.one>
2022-10-03 10:05:43 +0700

    iplm file

--- lib/sample/widget/picker/menu_picker_screen.dart
+++ lib/sample/widget/picker/menu_picker_screen.dart
@@ -1,11 +1,13 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:com.roy93group.flutter_tutorial/lib/common/const/DimenConstants.dart';
 import 'package:com.roy93group.flutter_tutorial/lib/util/UIUtils.dart';
 import 'package:com.roy93group.flutter_tutorial/sample/widget/picker/country_picker/country_picker_screen.dart';
-import 'package:com.roy93group.flutter_tutorial/sample/widget/picker/day_picker_screen.dart';
+import 'package:com.roy93group.flutter_tutorial/sample/widget/picker/day_picker/day_picker_screen.dart';
 import 'package:com.roy93group.flutter_tutorial/sample/widget/picker/image_picker/image_picker_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'file_picker/file_picker_demo.dart';
 
 /**
  * Created by Loitp on 08,August,2022
@@ -34,19 +36,25 @@ class MenuPickerScreen extends StatelessWidget {
             UIUtils.getButton(
               "CountryPickerScreen",
               () {
-                Get.to(CountryPickerScreen());
+                Get.to(() => CountryPickerScreen());
+              },
+            ),
+            UIUtils.getButton(
+              "FilePickerDemo",
+              () {
+                Get.to(() => FilePickerDemo());
               },
             ),
             UIUtils.getButton(
               "DayPickerScreen",
               () {
-                Get.to(DayPickerScreen());
+                Get.to(() => DayPickerScreen());
               },
             ),
             UIUtils.getButton(
               "ImagePickerScreen",
               () {
-                Get.to(ImagePickerScreen());
+                Get.to(() => ImagePickerScreen());
               },
             ),
           ],
