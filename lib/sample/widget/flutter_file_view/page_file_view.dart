7bd5a63c12c53d7b0ad49f3cd170cdb0f58a2cb9
loitp <loitp@galaxy.one>
2023-01-09 16:53:07 +0700

    update

---
+++ lib/sample/widget/flutter_file_view/page_file_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_file_view/flutter_file_view.dart';
+
+class FileViewPage extends StatefulWidget {
+  // ignore: public_member_api_docs
+  const FileViewPage({Key? key, required this.controller}) : super(key: key);
+
+  /// The [FileViewController] responsible for the file being rendered in this
+  /// widget.
+  final FileViewController controller;
+
+  @override
+  State<FileViewPage> createState() => _FileViewPageState();
+}
+
+class _FileViewPageState extends State<FileViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('文档')),
+      body: Column(
+        children: <Widget>[
+          Expanded(child: FileView(controller: widget.controller)),
+        ],
+      ),
+    );
+  }
+}
