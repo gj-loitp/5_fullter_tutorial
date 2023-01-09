47a30abf71939cd1dc71051c1d09e42d560f6fb4
loitp <loitp@galaxy.one>
2023-01-09 15:34:38 +0700

    update

--- lib/sample/widget/drop_shadow/drop_shadow_screen.dart
+++ lib/sample/widget/drop_shadow/drop_shadow_screen.dart
@@ -1,7 +1,7 @@
-import 'package:com.roy93group.flutter_tutorial/lib/common/const/dimen_constants.dart';
 import 'package:com.roy93group.flutter_tutorial/lib/core/base_stateful_state.dart';
 import 'package:com.roy93group.flutter_tutorial/lib/util/ui_utils.dart';
 import 'package:com.roy93group.flutter_tutorial/lib/util/url_launcher_utils.dart';
+import 'package:drop_shadow/drop_shadow.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -43,10 +43,51 @@ class _DropShadowScreenState extends BaseStatefulState<DropShadowScreen> {
               "https://github.com/tplloi/drop_shadow");
         },
       ),
-      body: ListView(
-        padding: EdgeInsets.all(DimenConstants.marginPaddingMedium),
-        physics: BouncingScrollPhysics(),
-        children: [],
+      body: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              DropShadow(
+                child: Image.network(
+                  'https://images.pexels.com/photos/1191639/pexels-photo-1191639.jpeg',
+                  width: 250,
+                ),
+              ),
+              const SizedBox(height: 35),
+              DropShadow(
+                child: Container(
+                  height: 150,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: const LinearGradient(
+                      colors: [Color(0xFF3366FF), Color(0xFF00CCFF)],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 35),
+              const DropShadow(
+                child: Text(
+                  'Ehuehuehueuhe',
+                  style: TextStyle(fontSize: 35, color: Colors.orange),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
