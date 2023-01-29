f9f4c752b26f682d69316f2055dc11b04b26413b
loitp <loitp@galaxy.one>
2023-01-29 20:06:49 +0700

    up

--- lib/main_menu_screen.dart
+++ lib/main_menu_screen.dart
@@ -164,6 +164,21 @@ class _MenuScreenState extends BaseStatefulState<MenuScreen> {
                   FirebaseCrashlytics.instance.crash();
                 },
               ),
+            SizedBox(height: DimenConstants.marginPaddingMedium),
+            InkWell(
+              onTap: () {
+                UrlLauncherUtils.moreApp();
+              },
+              child: UIUtils.getText(
+                  """People can criticise me all the time they want, by this is what I am and I won't change the way I live for them.
+I live the way I want to flow.
+If we don’t transform the world, who will? If not now, when?
+If you have something to give, give it now
+Do your little bit of good where you are.
+It’s those little bits of good put together that overwhelm the world.
+Don’t just think, do it. Now it is you turn,  do it now, download my apps and rate 5 stars. Thank you!
+https://play.google.com/store/apps/dev?id=9198084038765766736"""),
+            ),
           ],
         );
       }),
