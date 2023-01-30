802112ddc87d6d29f0a777136c58e9f2e9ae39f2
loitp <loitp@galaxy.one>
2023-01-30 10:16:13 +0700

    up

--- lib/sample/widget/webview/menu_webview_screen.dart
+++ lib/sample/widget/webview/menu_webview_screen.dart
@@ -1,11 +1,13 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:com.roy93group.flutter_tutorial/lib/common/const/dimen_constants.dart';
 import 'package:com.roy93group.flutter_tutorial/lib/util/ui_utils.dart';
 import 'package:com.roy93group.flutter_tutorial/sample/widget/webview/web_scraper/web_scraper_screen.dart';
 import 'package:com.roy93group.flutter_tutorial/sample/widget/webview/web_scraper/web_scraper_screen_2.dart';
 import 'package:com.roy93group.flutter_tutorial/sample/widget/webview/web_view_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'html/html_screen.dart';
 
 /**
  * Created by Loitp on 08,August,2022
@@ -31,6 +33,14 @@ class MenuWebviewScreen extends StatelessWidget {
           padding: EdgeInsets.all(DimenConstants.marginPaddingMedium),
           physics: BouncingScrollPhysics(),
           children: [
+            UIUtils.getButton(
+              "html",
+              () {
+                Get.to(HtmlScreen());
+              },
+              description:
+                  "APIs for parsing and manipulating HTML content outside the browser.",
+            ),
             UIUtils.getButton(
               "WebScraperScreen",
               () {
