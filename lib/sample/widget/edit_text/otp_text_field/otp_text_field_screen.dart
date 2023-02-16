ed7ac23c576fa1d7562870b949e991e0f71900bd
loitp <loitp@galaxy.one>
2023-02-16 20:53:10 +0700

    up

---
+++ lib/sample/widget/edit_text/otp_text_field/otp_text_field_screen.dart
@@ -0,0 +1,95 @@
+import 'package:com.roy93group.flutter_tutorial/lib/core/base_stateful_state.dart';
+import 'package:com.roy93group.flutter_tutorial/lib/util/ui_utils.dart';
+import 'package:com.roy93group.flutter_tutorial/lib/util/url_launcher_utils.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:otp_text_field/otp_field.dart';
+import 'package:otp_text_field/otp_text_field.dart';
+import 'package:otp_text_field/style.dart';
+/**
+ * Created by Loitp on 05,August,2022
+ * Galaxy One company,
+ * Vietnam
+ * +840766040293
+ * freuss47@gmail.com
+ */
+class OtpTextFieldScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _OtpTextFieldScreenState();
+  }
+}
+
+class _OtpTextFieldScreenState extends BaseStatefulState<OtpTextFieldScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: UIUtils.getAppBar(
+        "otp_text_field",
+        () {
+          Get.back();
+        },
+        () {
+          UrlLauncherUtils.launchInWebViewWithJavaScript(
+              "https://pub.dev/packages/otp_text_field");
+        },
+      ),
+      body: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, this.title}) : super(key: key);
+  final String? title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  OtpFieldController otpController = OtpFieldController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.cleaning_services),
+        onPressed: () {
+          print("Floating button was pressed.");
+          otpController.clear();
+          // otpController.set(['2', '3', '5', '5', '7']);
+          // otpController.setValue('3', 0);
+          // otpController.setFocus(1);
+        },
+      ),
+      body: Center(
+        child: OTPTextField(
+            controller: otpController,
+            length: 5,
+            width: MediaQuery.of(context).size.width,
+            textFieldAlignment: MainAxisAlignment.spaceAround,
+            fieldWidth: 45,
+            fieldStyle: FieldStyle.box,
+            outlineBorderRadius: 15,
+            style: TextStyle(fontSize: 17),
+            onChanged: (pin) {
+              print("Changed: " + pin);
+            },
+            onCompleted: (pin) {
+              print("Completed: " + pin);
+            }),
+      ),
+    );
+  }
+}
