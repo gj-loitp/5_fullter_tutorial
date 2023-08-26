a7500798ae83ff331d01583777e2dc2505af0660
roy <roy93group@gmail.com>
2023-08-26 19:48:49 +0700

    update

--- lib/sample/widget/grid/flutterStaggeredGridView/MenuFlutterStaggeredGridViewScreen.dart
+++ lib/sample/widget/grid/flutterStaggeredGridView/MenuFlutterStaggeredGridViewScreen.dart
@@ -1,25 +1,9 @@
 import 'package:com.roy93group.flutter_tutorial/lib/common/const/dimen_constants.dart';
 import 'package:com.roy93group.flutter_tutorial/lib/util/ui_utils.dart';
 import 'package:com.roy93group.flutter_tutorial/lib/util/url_launcher_utils.dart';
+import 'package:com.roy93group.flutter_tutorial/sample/widget/grid/flutterStaggeredGridView/sample/main.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-import 'Example1.dart';
-import 'Example2.dart';
-import 'Example3.dart';
-import 'Example4.dart';
-import 'Example5.dart';
-import 'Example6.dart';
-import 'Example7.dart';
-import 'Example8.dart';
-import 'SpannableCountCountPage.dart';
-import 'SpannableCountExtentPage.dart';
-import 'SpannableExtentCountPage.dart';
-import 'SpannableExtentExtentPage.dart';
-import 'StaggeredCountCountPage.dart';
-import 'StaggeredCountExtentPage.dart';
-import 'StaggeredExtentCountPage.dart';
-import 'StaggeredExtentExtentPage.dart';
 
 /**
  * Created by Loitp on 08,August,2022
@@ -49,99 +33,9 @@ class MenuFlutterStaggeredGridViewScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             UIUtils.getButton(
-              "Example01",
+              "Sample",
               () {
-                Get.to(Example01());
-              },
-            ),
-            UIUtils.getButton(
-              "Example02",
-              () {
-                Get.to(Example02());
-              },
-            ),
-            UIUtils.getButton(
-              "Example03",
-              () {
-                Get.to(Example03());
-              },
-            ),
-            UIUtils.getButton(
-              "Example04",
-              () {
-                Get.to(Example04());
-              },
-            ),
-            UIUtils.getButton(
-              "Example05",
-              () {
-                Get.to(Example05());
-              },
-            ),
-            UIUtils.getButton(
-              "Example06",
-              () {
-                Get.to(Example06());
-              },
-            ),
-            UIUtils.getButton(
-              "Example07",
-              () {
-                Get.to(Example07());
-              },
-            ),
-            UIUtils.getButton(
-              "Example08",
-              () {
-                Get.to(Example08());
-              },
-            ),
-            UIUtils.getButton(
-              "SpannableCountCountPage",
-              () {
-                Get.to(SpannableCountCountPage());
-              },
-            ),
-            UIUtils.getButton(
-              "SpannableCountExtentPage",
-              () {
-                Get.to(SpannableCountExtentPage());
-              },
-            ),
-            UIUtils.getButton(
-              "SpannableExtentCountPage",
-              () {
-                Get.to(SpannableExtentCountPage());
-              },
-            ),
-            UIUtils.getButton(
-              "SpannableExtentExtentPage",
-              () {
-                Get.to(SpannableExtentExtentPage());
-              },
-            ),
-            UIUtils.getButton(
-              "StaggeredCountCountPage",
-              () {
-                Get.to(StaggeredCountCountPage());
-              },
-            ),
-            UIUtils.getButton(
-              "StaggeredCountExtentPage",
-              () {
-                Get.to(StaggeredCountExtentPage());
-              },
-            ),
-            UIUtils.getButton(
-              "StaggeredExtentCountPage",
-              () {
-                Get.to(StaggeredExtentCountPage());
-              },
-            ),
-            UIUtils.getButton(
-              "StaggeredExtentExtentPage",
-              () {
-                Get.to(StaggeredExtentExtentPage());
+                Get.to(HomePageStaggeredGridView());
               },
             ),
           ],
